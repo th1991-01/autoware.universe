72f49718e440ab11b504c96e0361f1bd7cce8b30
Maxime CLEMENT <maxime.clement@tier4.jp>
2022-09-01 17:42:49 +0900

    Add trajectory_sampler_node (path+velocity planning) [WIP]
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- launch/tier4_planning_launch/launch/scenario_planning/lane_driving/motion_planning/sampling_based_motion_planning.launch.py
+++ launch/tier4_planning_launch/launch/scenario_planning/lane_driving/motion_planning/sampling_based_motion_planning.launch.py
@@ -21,7 +21,6 @@ from launch.actions import GroupAction
 from launch.actions import OpaqueFunction
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
-from launch.conditions import LaunchConfigurationEquals
 from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
@@ -48,37 +47,6 @@ def launch_setup(context, *args, **kwargs):
     )
     with open(common_param_path, "r") as f:
         common_param = yaml.safe_load(f)["/**"]["ros__parameters"]
-
-    # obstacle avoidance planner
-    obstacle_avoidance_planner_param_path = os.path.join(
-        get_package_share_directory("tier4_planning_launch"),
-        "config",
-        "scenario_planning",
-        "lane_driving",
-        "motion_planning",
-        "obstacle_avoidance_planner",
-        "obstacle_avoidance_planner.param.yaml",
-    )
-    with open(obstacle_avoidance_planner_param_path, "r") as f:
-        obstacle_avoidance_planner_param = yaml.safe_load(f)["/**"]["ros__parameters"]
-    obstacle_avoidance_planner_component = ComposableNode(
-        package="obstacle_avoidance_planner",
-        plugin="ObstacleAvoidancePlanner",
-        name="obstacle_avoidance_planner",
-        namespace="",
-        remappings=[
-            ("~/input/objects", "/perception/object_recognition/objects"),
-            ("~/input/path", LaunchConfiguration("input_path_topic")),
-            ("~/output/path", "obstacle_avoidance_planner/trajectory"),
-        ],
-        parameters=[
-            obstacle_avoidance_planner_param,
-            vehicle_info_param,
-            {"is_showing_debug_info": False},
-            {"is_stopping_if_outside_drivable_area": True},
-        ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-    )
 
     # surround obstacle checker
     surround_obstacle_checker_param_path = os.path.join(
@@ -119,107 +87,6 @@ def launch_setup(context, *args, **kwargs):
         extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
     )
 
-    # obstacle stop planner
-    obstacle_stop_planner_param_path = os.path.join(
-        get_package_share_directory("tier4_planning_launch"),
-        "config",
-        "scenario_planning",
-        "lane_driving",
-        "motion_planning",
-        "obstacle_stop_planner",
-        "obstacle_stop_planner.param.yaml",
-    )
-    obstacle_stop_planner_acc_param_path = os.path.join(
-        get_package_share_directory("tier4_planning_launch"),
-        "config",
-        "scenario_planning",
-        "lane_driving",
-        "motion_planning",
-        "obstacle_stop_planner",
-        "adaptive_cruise_control.param.yaml",
-    )
-    with open(obstacle_stop_planner_param_path, "r") as f:
-        obstacle_stop_planner_param = yaml.safe_load(f)["/**"]["ros__parameters"]
-    with open(obstacle_stop_planner_acc_param_path, "r") as f:
-        obstacle_stop_planner_acc_param = yaml.safe_load(f)["/**"]["ros__parameters"]
-    obstacle_stop_planner_component = ComposableNode(
-        package="obstacle_stop_planner",
-        plugin="motion_planning::ObstacleStopPlannerNode",
-        name="obstacle_stop_planner",
-        namespace="",
-        remappings=[
-            ("~/output/stop_reason", "/planning/scenario_planning/status/stop_reason"),
-            ("~/output/stop_reasons", "/planning/scenario_planning/status/stop_reasons"),
-            ("~/output/max_velocity", "/planning/scenario_planning/max_velocity_candidates"),
-            (
-                "~/output/velocity_limit_clear_command",
-                "/planning/scenario_planning/clear_velocity_limit",
-            ),
-            ("~/output/trajectory", "/planning/scenario_planning/lane_driving/trajectory"),
-            (
-                "~/input/pointcloud",
-                "/perception/obstacle_segmentation/pointcloud",
-            ),
-            ("~/input/objects", "/perception/object_recognition/objects"),
-            ("~/input/odometry", "/localization/kinematic_state"),
-            ("~/input/trajectory", "sampling_planner/trajectory"),
-        ],
-        parameters=[
-            common_param,
-            obstacle_stop_planner_param,
-            obstacle_stop_planner_acc_param,
-            vehicle_info_param,
-            {"enable_slow_down": False},
-        ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-    )
-
-    # obstacle cruise planner
-    obstacle_cruise_planner_param_path = os.path.join(
-        get_package_share_directory("tier4_planning_launch"),
-        "config",
-        "scenario_planning",
-        "lane_driving",
-        "motion_planning",
-        "obstacle_cruise_planner",
-        "obstacle_cruise_planner.param.yaml",
-    )
-    with open(obstacle_cruise_planner_param_path, "r") as f:
-        obstacle_cruise_planner_param = yaml.safe_load(f)["/**"]["ros__parameters"]
-    obstacle_cruise_planner_component = ComposableNode(
-        package="obstacle_cruise_planner",
-        plugin="motion_planning::ObstacleCruisePlannerNode",
-        name="obstacle_cruise_planner",
-        namespace="",
-        remappings=[
-            ("~/input/trajectory", "sampling_planner/trajectory"),
-            ("~/input/odometry", "/localization/kinematic_state"),
-            ("~/input/objects", "/perception/object_recognition/objects"),
-            ("~/output/trajectory", "/planning/scenario_planning/lane_driving/trajectory"),
-            ("~/output/velocity_limit", "/planning/scenario_planning/max_velocity_candidates"),
-            ("~/output/clear_velocity_limit", "/planning/scenario_planning/clear_velocity_limit"),
-            ("~/output/stop_reasons", "/planning/scenario_planning/status/stop_reasons"),
-        ],
-        parameters=[
-            common_param,
-            obstacle_cruise_planner_param,
-        ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-    )
-
-    obstacle_cruise_planner_relay_component = ComposableNode(
-        package="topic_tools",
-        plugin="topic_tools::RelayNode",
-        name="obstacle_cruise_planner_relay",
-        namespace="",
-        parameters=[
-            {"input_topic": "sampling_planner/trajectory"},
-            {"output_topic": "/planning/scenario_planning/lane_driving/trajectory"},
-            {"type": "autoware_auto_planning_msgs/msg/Trajectory"},
-        ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-    )
-
     sampler_node_param_path = os.path.join(
         get_package_share_directory("sampler_node"),
         "config",
@@ -230,12 +97,14 @@ def launch_setup(context, *args, **kwargs):
 
     sampler_node = Node(
         package="sampler_node",
-        # plugin='sampler_node::PathSamplerNode',
-        executable="path_sampler_node_exe",
-        name="path_sampler_node",
+        # plugin='sampler_node::TrajectorySamplerNode',
+        executable="trajectory_sampler_node_exe",
+        name="trajectory_sampler_node",
         namespace="",
         remappings=[
-            ("~/output/trajectory", "sampling_planner/trajectory"),
+            # ("~/output/trajectory", "sampling_planner/trajectory"),
+            # ("~/output/trajectory", "/planning/scenario_planning/lane_driving/trajectory"),
+            ("~/output/trajectory", "/planning/scenario_planning/trajectory"),
             ("~/input/objects", "/perception/object_recognition/objects"),
             ("~/input/steer", "/vehicle/status/steering_status"),
             ("~/input/path", LaunchConfiguration("input_path_topic")),
@@ -263,27 +132,7 @@ def launch_setup(context, *args, **kwargs):
         namespace="",
         package="rclcpp_components",
         executable=LaunchConfiguration("container_executable"),
-        composable_node_descriptions=[
-            obstacle_avoidance_planner_component,
-        ],
-    )
-
-    obstacle_stop_planner_loader = LoadComposableNodes(
-        composable_node_descriptions=[obstacle_stop_planner_component],
-        target_container=container,
-        condition=LaunchConfigurationEquals("cruise_planner", "obstacle_stop_planner"),
-    )
-
-    obstacle_cruise_planner_loader = LoadComposableNodes(
-        composable_node_descriptions=[obstacle_cruise_planner_component],
-        target_container=container,
-        condition=LaunchConfigurationEquals("cruise_planner", "obstacle_cruise_planner"),
-    )
-
-    obstacle_cruise_planner_relay_loader = LoadComposableNodes(
-        composable_node_descriptions=[obstacle_cruise_planner_relay_component],
-        target_container=container,
-        condition=LaunchConfigurationEquals("cruise_planner", "none"),
+        composable_node_descriptions=[],
     )
 
     surround_obstacle_checker_loader = LoadComposableNodes(
@@ -295,9 +144,6 @@ def launch_setup(context, *args, **kwargs):
     group = GroupAction(
         [
             container,
-            obstacle_stop_planner_loader,
-            obstacle_cruise_planner_loader,
-            obstacle_cruise_planner_relay_loader,
             surround_obstacle_checker_loader,
             sampler_node,
         ]
@@ -332,9 +178,6 @@ def generate_launch_description():
 
     # surround obstacle checker
     add_launch_arg("use_surround_obstacle_check", "true", "launch surround_obstacle_checker or not")
-    add_launch_arg(
-        "cruise_planner", "obstacle_stop_planner", "cruise planner type"
-    )  # select from "obstacle_stop_planner", "obstacle_cruise_planner", "none"
 
     add_launch_arg("use_intra_process", "false", "use ROS2 component container communication")
     add_launch_arg("use_multithread", "false", "use multithread")
